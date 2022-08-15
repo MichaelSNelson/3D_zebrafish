af43d69482b837c64ef238f2c8389ac2651ffbb3
MichaelSNelson <imagescientistwebsite@gmail.com>
2022-08-15 10:31:21 -0500

    First push

---
+++ addPointsLayers.py
@@ -0,0 +1,4 @@
+viewer.add_points(None, size=2, name="head", face_color="255,100,100", edge_color="255,255,255", symbol = "diamond")
+viewer.add_points(None, size=2, name="tail", face_color="255,0,255", edge_color="255,255,255", symbol = "diamond")
+viewer.add_points(None, size=2, name="spine", face_color="155,155,155", edge_color="255,255,255", symbol = "diamond")
+viewer.add_points(None, size=2, name="eyes", face_color="255,155,0", edge_color="255,255,255", symbol = "diamond")
